1bbcc724c4aa2ef38051b0b03e6bce766f19fb66
Lamberto Guevarra <seoblogger07@gmail.com>
2011-09-16 22:49:33 +0800

    rearrange menu

--- application/views/pageTemplate/top-menu.php
+++ application/views/pageTemplate/top-menu.php
@@ -8,13 +8,13 @@
 			<li><a href="<?php echo base_url();?>">Home</a></li>
 			<li><a href="#">Add</a>
 				<ul>
-					<li><a href="<?php echo base_url();?>cubicle/add">Cubicle</a></li>
+					<li><a href="<?php echo base_url();?>connector/add">Connector</a></li>
 					<li><a href="<?php echo base_url();?>cpu/add">CPU</a></li>
+					<li><a href="<?php echo base_url();?>cubicle/add">Cubicle</a></li>
+					<li><a href="<?php echo base_url();?>dialpad/add">Dial Pad</a></li>
 					<li><a href="<?php echo base_url();?>keyboard/add">Keyboard</a></li>
 					<li><a href="<?php echo base_url();?>mouse/add">Mouse</a></li>
 					<li><a href="<?php echo base_url();?>monitor/add">Monitor</a></li>
-					<li><a href="<?php echo base_url();?>dialpad/add">Dial Pad</a></li>
-					<li><a href="<?php echo base_url();?>connector/add">Connector</a></li>
 					<li><a href="<?php echo base_url();?>headset/add">Headset(Analog)</a></li>
 					<li><a href="<?php echo base_url();?>usb_headset/add">Headset(USB)</a></li>
 					<li><a href="<?php echo base_url();?>people/add">Headset(USB) Users</a></li>
@@ -23,13 +23,13 @@
 			</li>
 			<li><a href="#">View All</a>
 				<ul>
-					<li><a href="<?php echo base_url();?>cubicle/viewall">Cubicle</a></li>
+					<li><a href="<?php echo base_url();?>connector/viewall">Connector</a></li>
 					<li><a href="<?php echo base_url();?>cpu/viewall">CPU</a></li>
+					<li><a href="<?php echo base_url();?>cubicle/viewall">Cubicle</a></li>
+					<li><a href="<?php echo base_url();?>dialpad/viewall">Dial Pad</a></li>
 					<li><a href="<?php echo base_url();?>keyboard/viewall">Keyboard</a></li>
 					<li><a href="<?php echo base_url();?>mouse/viewall">Mouse</a></li>
 					<li><a href="<?php echo base_url();?>monitor/viewall">Monitor</a></li>
-					<li><a href="<?php echo base_url();?>dialpad/viewall">Dial Pad</a></li>
-					<li><a href="<?php echo base_url();?>connector/viewall">Connector</a></li>
 					<li><a href="<?php echo base_url();?>headset/viewall">Headset(Analog)</a></li>
 					<li><a href="<?php echo base_url();?>usb_headset/viewall">Headset(USB)</a></li>
 					<li><a href="<?php echo base_url();?>people/viewall">Headset(USB) Users</a></li>
